0e4acc08b218617686b2660a4af6451337d81b72
Kilian <dankestmirror@hotmail.es>
2026-04-10 17:34:42 +0100

    ValuesSave in console

--- js/validate.js
+++ js/validate.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('form');
 
-function validateForm() {
+function fundForm() {
     if(!form) {
         console.error('⛔ No se encuentra el Fomulario')
     }
@@ -8,4 +8,22 @@ function validateForm() {
     console.log('✅ Formulario encontrado')
 }
 
-validateForm()
+//Funcion que recoje todos los valores y los muestra en consola
+function inputValuesALL(){
+    const inputs = document.querySelectorAll('input, textarea')
+    inputs.forEach((input) => {
+      const inputValue = input.value
+      if (inputValue === "") {
+            console.log(`¡El campo ${input.getAttribute('name')} está vacío!`);
+        } else {
+            console.log(`Todo correcto con: ${input.getAttribute('name')} - ${inputValue}`);
+        }
+    })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+    inputValuesALL();
+});
+
+fundForm()
